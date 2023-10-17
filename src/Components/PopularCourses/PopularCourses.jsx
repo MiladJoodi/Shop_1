d1bc0e85983b4b037e05ff50bc87c3a425fe1e7e
Milad Joodi <winbeta.com@gmail.com>
2023-10-17 22:36:10 +0330

    change directory

---
+++ src/Components/PopularCourses/PopularCourses.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './PopularCourses.css'
+import SectionHeader from '../SectionHeader/SectionHeader'
+
+export default function PopularCourses() {
+  return (
+    <div className='popular'>
+        <div className='container'>
+            <SectionHeader
+            title='محبوب ترین دوره ها'
+            desc='دوره های محبوب بر اساس امتیاز دانشجوها'
+            />
+            
+        </div>
+    </div>
+    )
+}
---
+++ src/Components/PresellCourses/PresellCourses.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './PresellCourses.css'
+import SectionHeader from '../SectionHeader/SectionHeader'
+
+export default function PresellCourses() {
+  return (
+    <div className='popular'>
+        <div className='container'>
+            <SectionHeader
+            title='دوره های پیش فروش'
+            desc='اینجا میتونی دوره ها رو پیش خرید کنی'
+            />
+            
+        </div>
+    </div>
+    )
+}
