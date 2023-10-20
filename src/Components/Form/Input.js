b44b7650c93d58b32f0cbae763ec83056435eae3
Milad Joodi <winbeta.com@gmail.com>
2023-10-21 00:12:44 +0330

    ♥

--- src/Components/Form/Input.js
+++ src/Components/Form/Input.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import "./Input.css";
 
+const inputReducer = (state, action)=>{
+  switch(action.type){
+    case 'CHANGE' : {
+      return {
+        ...state,
+        value: action.value,
+        isValid: action.isValid
+      }
+    }
+    default :{
+      return state
+    }
+  }
+}
 
 export default function Input(props) {
 
+  const [mainInput, dispatch] = useReducer(inputReducer, {
+    value: '',
+    isValid: false
+  })
+
   const onChangeHandler = (event)=>{
     console.log(event.target.value)
+    dispatch({
+      type: 'CHANGE',
+      value: event.target.value,
+      isValid: true
+    })
   }
 
 
@@ -13,13 +37,15 @@ export default function Input(props) {
     <input
       type={props.type}
       placeholder={props.placeholder}
-      className={props.className}
+      className={`${props.className} ${mainInput.isValid ? 'success' : 'error'}`}
+      value={mainInput.value}
       onChange={onChangeHandler}
     />
   ) : (
     <textarea
       placeholder={props.placeholder}
-      className={props.className}
+      className={`${props.className} ${mainInput.isValid ? 'success' : 'error'}`}
+      value={mainInput.value}
     />
   )
 
