8f17a14d83a561610eadafb95639e4fc7b9895a1
Milad Joodi <winbeta.com@gmail.com>
2023-10-27 11:08:48 +0330

    Let's go to North.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react'
-import { useRoutes } from 'react-router-dom'
-import routes from './routes'
-import './App.css';
+import React, { useState } from "react";
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
+import "./App.css";
+import AuthContext from "./context/authContext";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(false);
+  const [userInfos, setUserInfos] = useState(false);
 
-  const router = useRoutes(routes)
+  const router = useRoutes(routes);
+
+  const login = (token)=> {
+    setToken(token)
+    // localStorage.setItem('user', {token: token}) 
+    //داخل لوکال استوریج نمیشه آبجکت ذخیره کردن برای همین میذاریم داخل جی سون استرینگی فای
+    // localStorage.setItem('user', JSON.stringify({token: token})) 
+  // اینجوری مینویسیم
+    localStorage.setItem('user', JSON.stringify({token})) 
+  }
 
   return (
-    <div>
-      { router }
-    </div>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        token: token,
+        userInfos: userInfos,
+        login: () => {},
+        logout: () => {},
+      }}
+    >
+      {router}
+    </AuthContext.Provider>
   );
 }
 
